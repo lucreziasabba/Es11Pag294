ff3919884368ec8608694d7f0f33a3235cc717c8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-25 09:40:16 +0100

    Add project files.

---
+++ ConsoleEsercizio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreriaEsercizio;
+
+namespace ConsoleEsercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TANTI = 10;
+            Random r = new Random();
+            int[] array = new int[TANTI];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i]= r.Next(1,10);
+            }
+            Console.WriteLine("Random: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            array = Classe.Ordinato(array);
+            Console.WriteLine("\nOrdinato:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibreriaEsercizio/Classe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaEsercizio
+{
+    public class Classe
+    {
+        public static int[] Ordinato(int[] array)
+        {
+            int[] arrayOrdinato = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int c = 0;
+                for (int y = 0; y < array.Length; y++)
+                {
+                    if (array[i] > array[y])
+                    {
+                        c++;
+                    }
+                }
+                arrayOrdinato[c] =array[i];
+            }
+            for (int i = 0; i < arrayOrdinato.Length; i++)
+            {
+                if (arrayOrdinato[i] == 0)
+                {
+                    arrayOrdinato[i] = arrayOrdinato[i - 1];
+                }
+            }
+            return arrayOrdinato;
+        }
+    }
+}
